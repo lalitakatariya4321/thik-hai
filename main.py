1aa5cc532d1918e7af4c02871b24dcf7ae100771
superman-enamy <47158027+superman-enamy@users.noreply.github.com>
2024-08-01 22:53:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,35 +3,34 @@ import json
 import re
 
 # Fetch the JSON data from the API
-response = requests.get('https://tplayapi.code-crafters.app/321codecrafters/fetcher.json')
+response = requests.get('https://fox.toxic-gang.xyz/tata/channels')
 input_json = response.json()
 
 # Function to perform replacements
 def replace_urls(channel):
-    if 'manifest_url' in channel:
+    if 'initialUrl' in channel:
         # Replace delta and bpweb with bpprod and tatasky.akamaized.net with catchup.akamaized.net
-        url = channel['manifest_url'].replace('delta', 'bpprod').replace('bpweb', 'bpprod').replace('tatasky.akamaized.net', 'catchup.akamaized.net')
+        url = channel['initialUrl'].replace('delta', 'bpprod').replace('bpweb', 'bpprod').replace('tatasky.akamaized.net', 'catchup.akamaized.net')
         # Replace <number>.akamaized.net with catchup.akamaized.net preserving the number
         url = re.sub(r'(\d+)\.akamaized\.net', r'\1catchup.akamaized.net', url)
         # Replace the ending with xxx.mpd
         url = re.sub(r'[a-zA-Z0-9]+\.mpd$', 'xxx.mpd', url)
-        channel['manifest_url'] = url
+        channel['initialUrl'] = url
     return channel
 
 # Replace URLs in all channels
-channels = input_json['data']['channels']
+channels = input_json['data']
 customized_channels = [replace_urls(channel) for channel in channels]
 
 # Prepare the final output format
 output_json = {}
-for i, channel in enumerate(customized_channels, 1):
-    if 'clearkeys' in channel and channel['clearkeys']:
-        clearkey = channel['clearkeys'][0]  # Use only the first clearkey
+for channel in customized_channels:
+    if 'licence1' in channel and 'licence2' in channel:
         key_info = {
-            'clearkey': clearkey['hex'],
-            'url': channel['manifest_url']
+            'clearkey': f"{channel['licence1']}:{channel['licence2']}",
+            'url': channel['initialUrl']
         }
-        output_json[f'{channel["id"]}'] = key_info
+        output_json[channel["id"]] = key_info
 
 # Save the output to a JSON file
 with open('pta_nhi.json', 'w') as outfile:
